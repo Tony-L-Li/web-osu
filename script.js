76cee9ab8e1262a839a5f8263b3592986f19ff0d
Tony Li <tonyli2048@gmail.com>
2017-05-17 20:02:35 -0400

    Changed hosting url for song and map files

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
 //test
 $(function () {
-  var songUrl = ['https://dl.dropboxusercontent.com/u/46950173/web_osu/m_normal.osu',
-  'https://dl.dropboxusercontent.com/u/46950173/web_osu/test.osu'];
+  var songUrl = ['https://www.student.cs.uwaterloo.ca/~ml3li/web_osu/m_normal.osu',
+  'https://www.student.cs.uwaterloo.ca/~ml3li/web_osu/test.osu'];
   $('.song').click(function () {
       $('.main-container').css( { 'opacity': 0, 'display': 'none'});
       $('body').append('<div class="game">');
       $.get(songUrl[$('.song').index(this)], function (data) {
-      var testOsu = new Engine($('.game'), parseNotes(parseOsu(data)), 'https://dl.dropboxusercontent.com/u/46950173/web_osu/test.mp3');
+      var testOsu = new Engine($('.game'), parseNotes(parseOsu(data)), 'https://www.student.cs.uwaterloo.ca/~ml3li/web_osu/test.mp3');
       testOsu.start(function () {
         $('.game').remove();
         $('.main-container').css( { 'opacity': 1, 'display': 'block'});
       });
     });  
   });
-  /*
-  
-*/
-  /*
-  $.get('https://dl.dropboxusercontent.com/u/46950173/web_osu/m_normal.osu', function (data) {
-    
-    var testOsu = new Engine($('.game'), parseNotes(parseOsu(data)), 'https://dl.dropboxusercontent.com/u/46950173/web_osu/test.mp3');
-    
-    testOsu.start();
-    });
-*/
 });
